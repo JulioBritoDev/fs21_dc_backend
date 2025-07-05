d98439662a5a2929414c15feea158a7a053b5be4
Márcio Ferreira <hmarciogferreira@gmail.com>
2025-07-05 10:38:07 -0300

    AJustes

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.get('/', (request, response) => {
 app.use(PublicRoute)
 app.use(PrivateRoute)
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log("Servidor executando na porta 3000");
 })
